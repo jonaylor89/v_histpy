7308c45184b4716fb26e9c71c3d113853a8c90e2
John Naylor <jonaylor89@gmail.com>
2019-03-28 01:07:03 -0400

    Write everything

--- src/v_hist.py
+++ src/v_hist.py
@@ -1,5 +1,6 @@
-#!/usr/bin/env bash
+#!/usr/bin/env python3
 
+import os
 from typing import List
 from random import randint
 
@@ -8,13 +9,31 @@ class Histogram(object):
     def __init__(self):
         self.labels: List[str] = []
         self.values: List[str] = []
+        self.label_len: int = 1
 
     def add(self, label: str, value: int):
-        labels.append(label)
-        values.append(value)
+        self.labels.append(label)
+        self.values.append(value)
+
+        if len(label) > self.label_len:
+            self.label_len = len(label)
 
     def show(self):
-        pass
+        max_value = max(self.values)
+        rows, columns = os.popen('stty size', 'r').read().split()
+        width = int(columns) - (self.label_len + 2)
+        dw = max_value / width
+
+        for l, v in zip(self.labels, self.values):
+            print("{label:{fill}>{width}}|".format(label=l, fill=' ', width=self.label_len), end='')
+
+            for x in range(width):
+                if v > dw * x:
+                    print("#", end='')
+                else:
+                    break;
+
+            print()
 
     def draw(self, values: List[int]):
         for i, value in enumerate(values):
